17c0be76297c272aa54da3a0392d080fe7732052
Prasun Banerjee <95117928+theprasunbanerjee@users.noreply.github.com>
2023-12-22 20:35:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('voting-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    var selectedOrganizer = document.getElementById('organizer').value;
+
+    // Check if an organizer is selected
+    if (selectedOrganizer) {
+        switch (selectedOrganizer) {
+            case 'rohit-saha':
+                window.location.href = 'https://www.google.com'; // Redirect to Google for Rohit Saha
+                break;
+            case 'rahul-burman':
+                // Handle redirect for Rahul Burman if needed
+                break;
+            case 'sayantan-dey':
+                // Handle redirect for Sayantan Dey if needed
+                break;
+            default:
+                // Handle default case if needed
+                break;
+        }
+    }
+});
